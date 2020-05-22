a9d22a888bedc1703629e5fd34f5ce30b92cb719
gesab001 <144000giovanni@gmail.com>
2020-05-23 05:13:54 +1200

    added resolve json module with ts-ignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+// @ts-ignore
+import hymnsData from '../assets/hymns.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hymns';
+  title = hymnsData['1'].title;
 }
