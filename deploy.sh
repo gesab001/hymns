0a990ad9b0c8ae8bd63b80dc3972768a6f0ffc0d
giovanni <gesab001@gmail.com>
2021-02-24 01:00:13 +0000

    fix slider

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-sudo git add .
+git add .
 
 echo message
 read varname
-sudo git commit -m "$varname"
-sudo git push --all
+git commit -m "$varname"
+git push --all
 
 ng deploy --base-href=/hymns/
 
